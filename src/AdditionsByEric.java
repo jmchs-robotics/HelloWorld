4647dd8f3df559ac0f755a83fef9af4c9f057c1c
Jon Hall <jhjh87@gmail.com>
2018-10-03 19:50:13 -0600

    fourth

--- src/AdditionsByEric.java
+++ src/AdditionsByEric.java
@@ -1,4 +1,4 @@
-
+// A different class in a different file can be called from main()
 public class AdditionsByEric {
 	public static void myMethod() {
 		System.out.println("This is a method run from a different file!");
